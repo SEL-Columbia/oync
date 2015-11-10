015ebf7812ac7c9b2ac32c2d337f82f8b58266c5
chrisnatali <chris.natali@gmail.com>
2015-11-10 18:17:15 -0500

    oync container runs

--- run-oync.sh
+++ run-oync.sh
@@ -3,8 +3,8 @@
 
 # Map docker-compose defined env variables to internal env variables
 # and write to env file for subsequent processes to read (i.e. cron jobs) 
-cat - /oync/oync.env <<EOF
-export OYNC_OSM_API_URL="$OYNC_ENV_OSM_API_URL"
+cat - > /oync/oync.env <<EOF
+export OYNC_OSM_API_URL="$OSM_API_URL"
 export OYNC_DB_HOST="$DB_PORT_5432_TCP_ADDR"
 export OYNC_DB_USER=postgres
 export OYNC_DB="$DB_ENV_POSTGRES_DB"
@@ -21,6 +21,15 @@ do
   sleep 1
 done
 
-osm2pgsql --host "$OYNC_DB_HOST" --database "$OYNC_DB" --username "$OYNC_DB_USER" --append --style /oync/oync.style --slim /oync/empty.osm --hstore-all --extra-attributes
+# if planet_osm_point exists, append (we don't want to overwrite) otherwise create new
+if psql -d  "$OYNC_DB" -h "$OYNC_DB_HOST" -U "$OYNC_DB_USER" -c '\d' | grep planet_osm_point > /dev/null;
+then
+  echo "osm db already instantiated"
+else
+  echo "setting up osm db..."
+  psql -d "$OYNC_DB" -h "$OYNC_DB_HOST" -U "$OYNC_DB_USER" -c "CREATE EXTENSION hstore;" 
+  osm2pgsql --host "$OYNC_DB_HOST" --database "$OYNC_DB" --username "$OYNC_DB_USER" --create --style /oync/oync.style --slim /oync/empty.osm --hstore-all --extra-attributes
+fi
+
 crontab /oync/oync.crt
 cron

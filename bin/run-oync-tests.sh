ee3a8aae8b5046544fd62a27f1c717083c4e4d7a
chrisnatali <chris.natali@gmail.com>
2015-12-02 11:08:20 -0500

    wait for test server to be available

--- bin/run-oync-tests.sh
+++ bin/run-oync-tests.sh
@@ -6,5 +6,17 @@
 ./bin/setup-oync.sh
 . .env
 
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 1; }
+
+# wait for test-server to start
+NUM_TRIES = 10
+cur_time = 0
+while ! curl $OYNC_OSM_API_URL/api/0.6/changesets > /dev/null 2>&1 
+do 
+  [ $cur_time -gt $WAIT_TIME ] && die "$OYNC_OSM_API_URL never came up after $NUM_TRIES tries"
+  sleep 1
+done
+
 # run tests
 ./test/test-oync.sh
